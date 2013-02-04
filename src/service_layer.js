b3357b5607ed9855148697dadf04adcfdab50028
mgibson <gusgibson@yahoo.com>
2013-02-04 16:18:53 -0500

    new selection manager handles selection & deselection ServiceLayer singleton fires data to listeners map panel listens to service layer for data, to del man for selection

--- src/service_layer.js
+++ src/service_layer.js
@@ -1,4 +1,14 @@
-function ServiceLayer (src, callback) {
+// SingletonPattern being used for ServiceLayer
+//var Singleton = (function () {
+//    var c, d, e;
+//    return {
+//        a: 5,
+//        b: function () { return c; }
+//    };
+//}) ();
+
+// Singleton pattern
+var ServiceLayer = (function () {
     //var data = null;
     //var order = null;
 
@@ -19,39 +29,58 @@ function ServiceLayer (src, callback) {
 
     var layer = null;
 
-    $.ajax ({
-        url: src,
-        dataType: 'json',
-        success: function (data) {
-            // Set the data
-            //data = response;
+    var dataListeners = [];
 
-            /* // Find an ordering, if possible
-            order = [];
-            for (var key in data.features[0].properties) {
-                if (!isNaN (Number (data.features[0].properties[key])))
-                    order.push (key);
+
+    var fireNewData = function(dataLayer) {
+        $.each(dataListeners,function(i,dl) { dl.newData(dataLayer); });
+    };
+
+    // Singleton public methods/properties
+    return {
+
+        addDataListener: function(dl) { dataListeners.push(dl); },
+
+        loadUrl: function(url) {
+            var that = this;
+            $.ajax ({
+                url: url,
+                dataType: 'json',
+                success: function (data) {
+                    // Set the data
+                    //data = response;
+                    /* // Find an ordering, if possible
+                       order = [];
+                       for (var key in data.features[0].properties) {
+                       if (!isNaN (Number (data.features[0].properties[key])))
+                       order.push (key);
+                       }
+                       order.sort ();*/
+                    layer = wiggle.io.GeoJSON (data);
+                    that.update(); // temp
+                    layer
+                        .style ('stroke', wiggle.util.fcolor (.3, .3, .3, 1.0))
+                        .style ('stroke-opacity', .75)
+                        .style ('fill-opacity', .8);
+                    //callback (layer);
+                    fireNewData(layer);
+                }
+            });
+        },
+
+        // rename this
+        change: function (key, value) {
+            settings[key] = value;
+        },
+
+        update: function () {
+            // temporary 'stubbing' of true ColorMap
+            for (var i = 0; i < settings['ramp'].length; i ++) {
+                layer.features ().quantile (settings['step'], i + 1, settings['ramp'].length).style ('fill', settings['ramp'][i]);
             }
-            order.sort ();*/
-            layer = wiggle.io.GeoJSON (data);
-            layer
-                .style ('stroke', wiggle.util.fcolor (.3, .3, .3, 1.0))
-                .style ('stroke-opacity', .75)
-                .style ('fill-opacity', .8);
-            callback (layer);
+            /*layer.features ().select (settings['query']).each (function (i, f) {
+              var val
+              });*/
         }
-    });
-
-    this.change = function (key, value) {
-        settings[key] = value;
-    };
-
-    this.update = function () {
-        for (var i = 0; i < settings['ramp'].length; i ++) {
-            layer.features ().quantile (settings['step'], i + 1, settings['ramp'].length).style ('fill', settings['ramp'][i]);
-        }
-        /*layer.features ().select (settings['query']).each (function (i, f) {
-            var val 
-        });*/
-    };
-};
+    }; } ());
+ // this executes the function for Singleton purposes
