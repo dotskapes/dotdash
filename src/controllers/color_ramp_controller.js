0915bc6feb7f47e45915fccf06418d9daed64def
mgibson <gusgibson@yahoo.com>
2013-02-21 17:26:04 -0500

    sag colors for the color ramps now there is a fair amount of view in here contemplating a view file but even the view has a tiny bit of controller in it as d3 lends itself to muddling the 2

--- src/controllers/color_ramp_controller.js
+++ src/controllers/color_ramp_controller.js
@@ -5,44 +5,83 @@ goog.require('PanelManager');
 
 var ColorRampController = function () {
 
+    var currentRampIndex = 0;
     var colorMap = function() { return ServiceLayer.colorMap; };
-
-    var chooser = $('.filter-button[name="'+ColorRampController.NAME+'"]');
+    var filterSelector = '#'+ColorRampController.NAME+'-filter';
+    var rampSvgId = 'ramp-svg';
 
     var init = function() {
-        render(); // view.render();
-        chooser.change(function(event) {
-            var value = $(event.target).attr('value');
-            newRamp(value);
+        render();
+    };
+
+    // a new ramp is being asked for
+    var newRamp = function(rampIndex) {
+        colorMap().setColorRamp(rampIndex);
+        // redraw panels with new colors & redraws selection
+        PanelManager.draw();
+        currentRampIndex = rampIndex;
+        // selection has probably changed, redraw
+        redraw();
+    };
+
+    // wipe out old, and render new. if sel has changed will reflect
+    var newColorMap = function() {
+        d3.select('#'+rampSvgId).remove();
+        render();
+    }
+
+    // should this go in crc view??? it has a tiny bit of controller in it
+    var render = function() {
+        var xMargin = 3;
+        var yMargin = 3;
+        var colorWidth = 20;
+        var colorHeight = colorWidth;
+        var colorRamps = ColorRamps.RAMPS;
+        var totalheight = colorRamps.length * (colorHeight + 2*yMargin);
+        var rampWidth = ColorRamps.NUM_COLORS * colorWidth;
+        var totalwidth =  rampWidth + 2*xMargin;
+
+        // svg container
+        var svg = d3.select(filterSelector).
+            append("svg:svg").
+            attr('id',rampSvgId).
+            attr("width", totalwidth).
+            attr("height", totalheight);
+
+        var x = 0;
+        var y = 0;
+        $.each(colorRamps,function(rampIndex,ramp) {
+
+            // wipe out old selection with white background color
+            var rect = svg.append("svg:rect").
+                attr("x", x).
+                attr("y", y).
+                attr("height", colorHeight + 2*yMargin).
+                attr("width", rampWidth + 2*xMargin).
+                attr('fill','white');
+
+            // highlight current selected ramp
+            if (rampIndex == currentRampIndex) rect.attr('fill', 'yellow');
+
+            // draw color boxes, listen for mouse click
+            x = xMargin;
+            $.each(ramp,function(i,color) {
+                svg.append("svg:rect").
+                    attr("x", x).
+                    attr("y", y + yMargin).
+                    attr("height", colorHeight).
+                    attr("width", colorWidth).
+                    attr('fill', d3.rgb(color.rgb())).
+                    data([ramp]).
+                    on('click', function(d,i) {newRamp(rampIndex);});
+
+                x += colorWidth;
+            });
+            y += colorHeight + 2*yMargin;
+            x = 0;
         });
     };
 
-    var newRamp = function(value) {
-        colorMap().setColorRamp(value);
-        // redraw panels with new colors & redraws selection
-        PanelManager.draw();
-    };
-
-    // this should go in crc view i think ???
-    var render = function() {
-        //var template = Handlebars.templates['color_ramp'];
-        //var html = template({});
-        //filterElement().html(html);
-        var rect = d3.select(filterSelector).
-            append("svg:svg").
-            attr("width", 200).
-            attr("height", 30);
-
-        rect.append("svg:rect").
-            attr("x", 10).
-            attr("y", 0).
-            attr("height", 30).
-            attr("width", 80);
-    };
-
-    var filterElement = $(filterSelector);
-
-    var filterSelector = '#'+ColorRampController.NAME+'-filter';
 
     init();
 
