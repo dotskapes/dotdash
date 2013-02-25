5f81de3885b883542f046a8c1fe9dda35f059411
mgibson <gusgibson@yahoo.com>
2013-02-25 15:50:05 -0500

    range filter now working

--- src/controllers/distrib_range_controller.js
+++ src/controllers/distrib_range_controller.js
@@ -5,10 +5,18 @@ goog.require('ServiceLayer');
 var DistribRangeController = function() {
 
     var init = function() {
+
         var distrib = $('.filter-button[name="'+DistribRangeController.DISTRIB+'"]');
         distrib.change(function (event) {
             var value = parseInt($(event.target).val(),10);
             ServiceLayer.colorMap.dist(value);
+            panelManager.redraw();
+        });
+
+        var range = $('.filter-button[name="'+DistribRangeController.RANGE+'"]');
+        range.change(function (event) {
+            var value = parseInt($(event.target).val(),10);
+            ServiceLayer.colorMap.range(value);
             panelManager.redraw();
         });
     };
@@ -18,3 +26,4 @@ var DistribRangeController = function() {
 };
 
 DistribRangeController.DISTRIB = 'dist';
+DistribRangeController.RANGE = 'range';
