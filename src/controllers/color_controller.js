4481ca2fbaab115759a356329ce3b9cea9d9b64b
Amy Slagle <slagle@ehatek.org>
2013-04-22 11:33:12 -0400

    Bind to this for color controller radio button changes.

--- src/controllers/color_controller.js
+++ src/controllers/color_controller.js
@@ -51,13 +51,13 @@ var ColorController = Backbone.View.extend({
 
     initControllers : function (dashState, aggregateModel, serviceLayer) {
 
-        var onChange = function (callback) {
+        var onChange = _.bind(function (callback) {
             this.$('.filter-button').change(function (event) {
                 var name = $(event.target).attr('name');
                 var value = $(event.target).attr('value');
                 callback(name, value);
             });
-        };
+        }, this);
 
         // take this out and just have events go direct to controller?
         onChange(function (name, value) {
